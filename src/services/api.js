54504cbe892bc09328da76c230244140f0112d74
Spencer Kitson <spencer@kitsongroup.com>
2020-04-04 14:47:21 +0200

    api baseURL change

--- src/services/api.js
+++ src/services/api.js
@@ -1,19 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'https://sundaerun.appspot.com/api'
+  baseURL: 'http://test.fuseclients.com/api'
 },)
-
-// instance.interceptors.request.use(
-//   async (config) => {
-//     // if(slug){
-//     //   config.baseURL = `http://test.fuseclients.com/api/blog/${slug}`;
-//     // }
-//     return config;
-//   },
-//   (err) => {
-//     return Promise.reject(err);
-//   }
-// )
 
 export default instance;
